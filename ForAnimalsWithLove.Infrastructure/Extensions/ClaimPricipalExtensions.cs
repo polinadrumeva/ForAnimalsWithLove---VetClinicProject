3008c3639a4d710099e4af8aa93f69a848b56841
Polina <polina9130@gmail.com>
2023-08-03 16:59:42 +0300

    ADD

---
+++ ForAnimalsWithLove.Infrastructure/Extensions/ClaimPricipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace ForAnimalsWithLove.Infrastructure.Extensions
+{
+    public static class ClaimPricipalExtensions
+    {
+        public static string? GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
