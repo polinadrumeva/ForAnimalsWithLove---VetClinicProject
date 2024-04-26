f97fe9e867c12ced71e48e5df08f1a8e81cf042d
Polina <polina9130@gmail.com>
2024-04-26 15:37:33 +0300

    Add function for calculate actual age

--- ForAnimalsWithLove.Data.Service/Services/AdminService.cs
+++ ForAnimalsWithLove.Data.Service/Services/AdminService.cs
@@ -6,6 +6,7 @@ using ForAnimalsWithLove.ViewModels.Animals;
 using ForAnimalsWithLove.ViewModels.Enums;
 using ForAnimalsWithLove.ViewModels.IndexModels;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace ForAnimalsWithLove.Data.Service.Services
 {
@@ -332,6 +333,31 @@ namespace ForAnimalsWithLove.Data.Service.Services
 		{
 			var animal = await dbContext.Animals.FirstAsync(x => x.Id.ToString() == id);
 			var owner = await dbContext.Owners.FirstOrDefaultAsync(x => x.Id == animal.OwnerId);
+
+			if (owner == null)
+			{
+				return new AdminAnimalModel()
+				{
+					Id = animal.Id.ToString(),
+					Name = animal.Name,
+					Age = animal.Age,
+					Photo = animal.Photo,
+					KindOfAnimal = animal.KindOfAnimal,
+					Breed = animal.Breed,
+					Color = animal.Color,
+					Sex = animal.Sex.ToString(),
+					Birthdate = animal.Birthdate,
+					DoesHasOwner = animal.DoesHasOwner,
+					OwnerId = null,
+					Owner = new AdminOwnerModel
+					{
+						FirstName = null,
+						LastName = null,
+						PhoneNumber = null,
+						Address = null
+					}
+				};
+			}
 
 			return new AdminAnimalModel()
 			{
